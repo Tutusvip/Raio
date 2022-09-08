ece8d2534261fe6b38c79e5e372e9ce1882867f2
tutusvip <111453250+Tutusvip@users.noreply.github.com>
2022-09-08 12:44:34 -0300

    Add files via upload

---
+++ media.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	float n1,n2,n3,media;
+	
+	printf("Digite o numero 1: ");
+	scanf("%f", &n1);
+	printf("Digite o numero 2: ");
+	scanf("%f", &n2);
+	printf("Digite o numero 3: ");
+	scanf("%f", &n2);
+	
+	media = (n1+n2+n3)/3;
+	
+	printf("media: %2.f", media);
+	
+}
